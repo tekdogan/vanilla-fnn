60abeedc89c83c5e3960dc2db2e29c7856868a23
tekdogan <tahatekdogan97@hotmail.com>
2018-02-09 14:41:18 +0300

    Initialized Hyper-parameters

--- fnn.py
+++ fnn.py
@@ -3,3 +3,10 @@ import torch.nn as nn
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 from torch.autograd import Variable
+
+input_size = 784      # Image size is 28 x 28 = 784
+hidden_size = 500
+num_classes = 10
+num_epochs = 5
+batch_size = 100
+learning_rate = 0.001
