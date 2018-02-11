044bea0959209812942543486bec2f5a7281e202
tekdogan <tahatekdogan97@hotmail.com>
2018-02-11 21:29:42 +0300

    Downloaded MNIST dataset and loaded it into code

--- fnn.py
+++ fnn.py
@@ -1,12 +1,34 @@
+#Import PyTorch
 import torch
 import torch.nn as nn
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 from torch.autograd import Variable
 
+#Initialize Hyper-parameters
 input_size = 784      # Image size is 28 x 28 = 784
 hidden_size = 500
 num_classes = 10
 num_epochs = 5
 batch_size = 100
 learning_rate = 0.001
+
+#Downloading MNIST dataset
+train_dataset = dsets.MNIST(root='./data',
+                           train=True,
+                           transform=transforms.ToTensor(),
+                           download=True)
+
+test_dataset = dsets.MNIST(root='./data',
+                           train=False,
+                           transform=transforms.ToTensor())
+
+
+#And then loading it into our code
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                          batch_size=batch_size,
+                                          shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          batch_size=batch_size,
+                                          shuffle=False)
