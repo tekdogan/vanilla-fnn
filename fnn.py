8b9367f1bb989a4ac7d0ec4fc00ff01a0dede76f
tekdogan <tahatekdogan97@hotmail.com>
2018-02-17 23:06:18 +0300

    Net class added (FNN)

--- fnn.py
+++ fnn.py
@@ -32,3 +32,18 @@ train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
                                           batch_size=batch_size,
                                           shuffle=False)
+
+
+#Constructing FNN
+class Net(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(Net, self).__init__()                    # Inherited from the parent class nn.Module
+        self.fc1 = nn.Linear(input_size, hidden_size)  # 1st Full-Connected Layer: 784 (input data) -> 500 (hidden node)
+        self.relu = nn.ReLU()                          # Non-Linear ReLU Layer: max(0,x)
+        self.fc2 = nn.Linear(hidden_size, num_classes) # 2nd Full-Connected Layer: 500 (hidden node) -> 10 (output class)
+    
+    def forward(self, x):                              # Forward pass: stacking each layer together
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        return out
